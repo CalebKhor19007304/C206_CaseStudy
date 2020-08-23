1792f2468859b5582584fdbe701cab6f55760ea2
19007304 <19007304@DESKTOP-45MJTON>
2020-08-24 01:42:55 +0800

    remove setters

--- C206_CaseStudy/src/Member.java
+++ C206_CaseStudy/src/Member.java
@@ -20,47 +20,32 @@ public class Member {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
 
 	public char getGender() {
 		return gender;
 	}
 
-	public void setGender(char gender) {
-		this.gender = gender;
-	}
+
 
 	public int getMobile() {
 		return mobile;
 	}
 
-	public void setMobile(int mobile) {
-		this.mobile = mobile;
-	}
 
 	public String getEmail() {
 		return email;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+
 
 	public String getDOB() {
 		return DOB;
 	}
 
-	public void setDOB(String dOB) {
-		DOB = dOB;
-	}
 
 	public String getCountry() {
 		return country;
 	}
 
-	public void setCountry(String country) {
-		this.country = country;
-	}
+
 }

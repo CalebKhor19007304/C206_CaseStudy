f63acb9575f663a9bffd626aa4809f2ba2ebc6bb
19007304 <19007304@DESKTOP-45MJTON>
2020-08-24 13:17:16 +0800

    added register course class

---
+++ C206_CaseStudy/src/RegisterCourse.java
@@ -0,0 +1,54 @@
+import java.util.Date;
+public class RegisterCourse {
+	private int registrationNo;
+    private int course_schedule_id;
+    private String mail;
+    private String status;
+    private Date registration_date;
+    
+    public RegisterCourse(int registrationNo, int course_schedule_id, String mail, String status,
+            Date registration_date) {
+        this.registrationNo = registrationNo;
+        this.course_schedule_id = course_schedule_id;
+        this.mail = mail;
+        this.status = status;
+        this.registration_date = registration_date;
+    }
+
+ 
+
+    public int getRegistrationNo() {
+        return registrationNo;
+    }
+
+ 
+
+    public int getCourse_schedule_id() {
+        return course_schedule_id;
+    }
+
+ 
+
+    public String getMail() {
+        return mail;
+    }
+
+ 
+
+    public String getStatus() {
+        return status;
+    }
+
+ 
+
+    public Date getRegistration_date() {
+        return registration_date;
+        
+    }
+    
+    
+
+ 
+
+}
+}
